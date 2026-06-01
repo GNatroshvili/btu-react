dbdcbd694834af270cbb6e9c455ad469f6b38c99
GNatroshvili <gigi.natroshvili@gmail.com>
2026-06-02 01:47:09 +0400

    feat: add edit and delete icons for notes

--- src/components/Note/Note.tsx
+++ src/components/Note/Note.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import styles from "./Note.module.scss";
 import DividerLine from "../DividerLine/DividerLine";
 import Checkbox from "../Checkbox/Checkbox";
+import editIcon from "../../assets/icons/edit.svg";
+import deleteIcon from "../../assets/icons/delete.svg";
 
 type Props = {
   title: string;
@@ -12,14 +14,21 @@ const Note = (props: Props) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.content}>
-        <Checkbox checked={isChecked} onChange={setIsChecked} />
-        <h3
-          className={`${styles.title} ${isChecked ? styles.strikethrough : ""}`}
-        >
-          {props.title}
-        </h3>
+      <div className={styles.contentWrapper}>
+        <div className={styles.content}>
+          <Checkbox checked={isChecked} onChange={setIsChecked} />
+          <h3
+            className={`${styles.title} ${isChecked ? styles.strikethrough : ""}`}
+          >
+            {props.title}
+          </h3>
+        </div>
+        <div className={styles.buttons}>
+          <img src={editIcon} alt="Edit" className={styles.button} />
+          <img src={deleteIcon} alt="Delete" className={styles.button} />
+        </div>
       </div>
+
       <DividerLine />
     </div>
   );
